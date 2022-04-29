58fcd56917e118222ed09084b204a9c7f95dc677
unknown <mikes0174@hotmail.com>
2022-04-29 10:59:48 -0500

    Navigation Bar

---
+++ HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import './styles';
+import styles from './styles';
+
+
+function HomeScreen(props) {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
