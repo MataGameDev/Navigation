f55ec97f222be6d793eee10209f72b6464206f0f
MataGitUniat <100615515+MataGitUniat@users.noreply.github.com>
2022-05-07 07:33:34 -0500

    Update HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {Button,View, Text} from 'react-native';
-import styles from './styles';
-
+import {Button, View, Text} from 'react-native'; 
+import styles from './style';
 
 function HomeScreen({navigation}) {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Home</Text>
-            <Button 
-                title="Ir a Menu"
-                onPress={() => navigation.navigate('Menu')}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+        <Text style={styles.texto}> Home </Text>
+        <Button 
+              title='Ir a menu'
+              onPress={() => navigation.navigate('Menu') }
+        />
+    </View>
+  );
 }
 
 export default HomeScreen;
