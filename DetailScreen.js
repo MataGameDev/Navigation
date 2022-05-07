fea7a9b02a7ec3526a28a98a3b720e2981af3384
MataGitUniat <100615515+MataGitUniat@users.noreply.github.com>
2022-05-07 10:54:14 -0500

    Update DetailScreen.js

--- DetailScreen.js
+++ DetailScreen.js
@@ -3,14 +3,14 @@ import {Button,View, Text} from 'react-native';
 import styles from './style';
 
 function DetailScreen({route, navigation}){
-  const {id, description} = route.params;
+  const {id, descripcion} = route.params;
   return(
   <View style={styles.container}>
     <Text style={styles.texto}>Tarea #{id} </Text>
-    <Text style={styles.texto}>Descripion: {description} </Text>
+    <Text style={styles.texto}>descripcion: {descripcion} </Text>
         <View style={styles.footer}>
           <Button
-             title='Volver'
+             title='Atras'
              onPress={() => navigation.goBack() }
           />
 
