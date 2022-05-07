0bd245061480c7a9f69f911c8d55e4fa108d365b
MataGitUniat <100615515+MataGitUniat@users.noreply.github.com>
2022-05-07 11:19:44 -0500

    Update Box.js

--- components/Box.js
+++ components/Box.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Text, View} from 'react-native';
 import PropTypes from 'prop-types'
-import styles from '../style';
+import styles from '../styles';
 
 
 const Box = ({children}) => {
