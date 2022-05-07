05399f42be4f3fb1c666b04936b0bf515787653f
MataGitUniat <100615515+MataGitUniat@users.noreply.github.com>
2022-05-07 10:55:24 -0500

    Update styles.js

--- styles.js
+++ styles.js
@@ -5,8 +5,7 @@ export default StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "green",
-      
+        backgroundColor: "white",
     },
 
     text: {marginBottom: 10,},
@@ -16,33 +15,26 @@ export default StyleSheet.create({
         height:80,
         justifyContent: 'center',
         alignItems: 'center',
-      //  alignSelf: 'stretch',
         borderWidth: 1,
-        backgroundColor: '#81c784',
+        backgroundColor: '#2599CD',
         margin: 5,
         
     },
-
     boxText: {
         color: 'darkslategray',
         fontWeight: 'bold',
     },
-
     row:{
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-around',
-        alignSelf: 'stretch',
-     
-        
+        alignSelf: 'stretch',     
     },
-
-   
     footer:{
         justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
-        backgroundColor: '#1b5e20',
+        backgroundColor: '#2599CD',
         borderWidth: 1,
         margin: 10,
         padding: 15,
@@ -52,9 +44,7 @@ export default StyleSheet.create({
     
         position: 'absolute',
         bottom: 0
-
     },
-
     containerButton:{
         flexDirection: 'row',
         justifyContent: 'space-around',
@@ -68,17 +58,16 @@ export default StyleSheet.create({
         height: 100,
     },
     titulo:{
-        color: '#01579b',
+        color: '#386C83',
         fontSize: 80,
         fontWeight: "bold",
       
     },
     texto:{
-        color: '#01579b',
+        color: '#386C83',
         fontSize: 25,
         fontWeight: "bold",
         margin: 10,
         bottom: 30
     }
-
 })
